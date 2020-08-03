60ecc72edf05b601cdee6a507c4f403fa1b72c7d
Sima <sima.boreiri@gmail.com>
2020-08-03 13:17:48 +0200

    creates new files for logic and ui for rps game, adds one more test for the title, adds content to html file

--- cypress/integration/userCanPlayRPS.feature.js
+++ cypress/integration/userCanPlayRPS.feature.js
@@ -1,5 +1,6 @@
 describe("user can play RPS", () => {
     it("user can see the options to choose", () => {
       cy.visit("http://localhost:3001")
+      cy.contains("Rock Paper Scissors")
     })
 })

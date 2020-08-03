2640cb5451e992e6f5cd02fee31f3b1d7817696f
Sima <sima.boreiri@gmail.com>
2020-08-03 13:31:23 +0200

    adds the new test for the score board and adds the score section for computer and player on Html file

--- cypress/integration/userCanPlayRPS.feature.js
+++ cypress/integration/userCanPlayRPS.feature.js
@@ -3,5 +3,7 @@ describe("user can play RPS", () => {
       cy.visit("http://localhost:3001")
       cy.contains("Rock Paper Scissors")
       cy.get('button#restart').contains('Restart Game').click()
+      cy.get("p#score-1").contains("Player: 0")
+      cy.get("p#score-2").contains("Computer: 0")
     })
 })

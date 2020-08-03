75b2eb68586fb8373547be40378768bfcabb65f3
Sima <sima.boreiri@gmail.com>
2020-08-03 13:48:32 +0200

    adds one line of test to display make your selection to the player and add the line to html file

--- cypress/integration/userCanPlayRPS.feature.js
+++ cypress/integration/userCanPlayRPS.feature.js
@@ -5,5 +5,6 @@ describe("user can play RPS", () => {
       cy.get('button#restart').contains('Restart Game').click()
       cy.get("p#score-1").contains("Player: 0")
       cy.get("p#score-2").contains("Computer: 0")
+      cy.contains("Make Your Selection")
     })
 })

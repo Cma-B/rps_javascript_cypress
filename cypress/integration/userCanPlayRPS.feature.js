3cfd56c84a66de17f4593aeb0732d2b44aca2c3f
Sima <sima.boreiri@gmail.com>
2020-08-03 13:24:10 +0200

    adds the new test for the restart game button, creates new file for styling and adds button to th eindex.html file

--- cypress/integration/userCanPlayRPS.feature.js
+++ cypress/integration/userCanPlayRPS.feature.js
@@ -2,5 +2,6 @@ describe("user can play RPS", () => {
     it("user can see the options to choose", () => {
       cy.visit("http://localhost:3001")
       cy.contains("Rock Paper Scissors")
+      cy.get('button#restart').contains('Restart Game').click()
     })
 })

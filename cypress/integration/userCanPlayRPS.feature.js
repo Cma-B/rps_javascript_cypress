c410a3b8fd9ffa4e25f670c932d95b1e3e293bc5
Sima <sima.boreiri@gmail.com>
2020-08-03 17:00:20 +0200

    adds test for the restart button, modifies the style of buttons and grids, adds functions and define variables in UI file, modifies html file by adding id of choices to the icon div

--- cypress/integration/userCanPlayRPS.feature.js
+++ cypress/integration/userCanPlayRPS.feature.js
@@ -1,13 +1,17 @@
 describe("user can play RPS", () => {
     it("user can see the options to choose", () => {
       cy.visit("http://localhost:3001")
-      cy.contains("Rock Paper Scissors")
-      cy.get('button#restart').contains('Restart Game').click()
+      cy.contains("Let's Play Rock Paper Scissors")
       cy.get("p#score-1").contains("Player: 0")
       cy.get("p#score-2").contains("Computer: 0")
       cy.contains("Make Your Selection")
       cy.get('i#rock').should('be.visible')
       cy.get('i#paper').should('be.visible')
       cy.get('i#scissors').should('be.visible')
+    })
+
+   it(" displays restart button after first click on each icon", () => {
+     cy.get("#choices").click()
+     cy.get("button#restart").contains("Restart Game").click()
     })
 })

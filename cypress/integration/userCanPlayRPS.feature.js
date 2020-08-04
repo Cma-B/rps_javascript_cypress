e9338801b9b617477b0005e54b279a6ef7448d48
Sima <sima.boreiri@gmail.com>
2020-08-04 13:07:23 +0200

    modifies the test foe each icon , remove modal and put the result as a simple text, modifies the index html file

--- cypress/integration/userCanPlayRPS.feature.js
+++ cypress/integration/userCanPlayRPS.feature.js
@@ -5,13 +5,16 @@ describe("user can play RPS", () => {
       cy.get("p#score-1").contains("Player: 0")
       cy.get("p#score-2").contains("Computer: 0")
       cy.contains("Make Your Selection")
-      cy.get('i#rock').should('be.visible')
-      cy.get('i#paper').should('be.visible')
-      cy.get('i#scissors').should('be.visible')
+      cy.get('i#rock').should('be.visible').click()
+      cy.get('i#paper').should('be.visible').click()
+      cy.get('i#scissors').should('be.visible').click()
     })
 
-   it(" displays restart button after first click on each icon", () => {
-     cy.get("#choices").click()
-     cy.get("button#restart").contains("Restart Game").click()
-    })
+   //it(" displays restart button after first click on each icon", () => {
+     //cy.get("#choices").click()
+     //cy.get("button#restart").contains("Restart Game")
+     //cy.get("p#score-1").contains("Player: 0")
+    // cy.get("p#score-2").contains("Computer: 0")
+   // })
+
 })
